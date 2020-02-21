36fd857a19444bcdff6d6ae0ac1317def92b1f3d
Chanjong Baek <readandaction@gmail.com>
2020-02-21 09:27:24 +0900

    Set middleware(cookie,body-parser)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -8,6 +10,9 @@ const PORT = 4000;
 const listening = () => {
   console.log(`Listening https://localhost${PORT}`);
 };
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("tiny"));
 

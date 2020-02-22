27b97888912a4d70f7ae95b92ef8462b07911cdd
Chanjong Baek <readandaction@gmail.com>
2020-02-22 13:23:25 +0900

    Fix error, in objecitive '=' = ':'

--- routes.js
+++ routes.js
@@ -14,21 +14,21 @@ const VIDEOS = "/videos",
   EDIT_VIDEO = "/:id/edit-video",
   DELETE_VIDEO = "/:id/delete-video";
 
-const routes ={
-    //global
-    home=HOME,
-    login=LOGIN,
-    join=JOIN,
-    //USERS
-    users= USERS,
-    userId =USER_ID,
-    editProfile= EDIT_PROFILE,
-    upload=UPLOAD,
-    logout=LOGOUT,
-    //VIDEO
-    videos =VIDEOS,
-    videoId=VIDEO_ID,
-    editVideo=EDIT_VIDEO,
-    deleteVideo=DELETE_VIDEO
-}
+const routes = {
+  //global
+  home: HOME,
+  login: LOGIN,
+  join: JOIN,
+  //USERS
+  users: USERS,
+  userId: USER_ID,
+  editProfile: EDIT_PROFILE,
+  upload: UPLOAD,
+  logout: LOGOUT,
+  //VIDEO
+  videos: VIDEOS,
+  videoId: VIDEO_ID,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
+};
 export default routes;

924e4a73704da230d1ae5e2c8d0ca968ab3489ce
Chanjong Baek <readandaction@gmail.com>
2020-02-22 12:48:20 +0900

    Error no module-app

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+
 const app = express();
 
-const PORT = 4000;
-
-const listening = () => {
-  console.log(`Listening https://localhost${PORT}`);
-};
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("tiny"));
 
-app.listen(PORT, listening);
-
-app.get("/", (req, res) => res.send("Home"));
-app.get("/profile", (req, res) => res.send("Profile"));
+export default app;
---
+++ init.js
@@ -0,0 +1,12 @@
+import app from "app";
+
+const PORT = 4000;
+
+const listening = () => {
+  console.log(`Listening https://localhost${PORT}`);
+};
+
+app.listen(PORT, listening);
+
+app.get("/", (req, res) => res.send("Home"));
+app.get("/profile", (req, res) => res.send("Profile"));

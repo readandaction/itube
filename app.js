630e169de94c1db8fd95e2a2f3c85855066ee8a5
Chanjong Baek <readandaction@gmail.com>
2020-02-25 11:44:21 +0900

    Make localsMiddleware

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
 import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -18,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.videos, videoRouter);
 app.use(routes.users, userRouter);

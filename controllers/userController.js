90ff5e514bcd334ec80bf8b304ac6b750526051f
Chanjong Baek <readandaction@gmail.com>
2020-02-25 11:49:18 +0900

    Make variables in controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const join = (req, res) => res.render("join");
-export const users = (req, res) => res.render("users");
-export const userDetail = (req, res) => res.render("userId");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changen Password");
+export const login = (req, res) => res.render("login", { pagetitle: "login" });
+export const logout = (req, res) =>
+  res.render("logout", { pagetitle: "logout" });
+export const join = (req, res) => res.render("join", { pagetitle: "join" });
+export const users = (req, res) => res.render("users", { pagetitle: "users" });
+export const userDetail = (req, res) =>
+  res.render("userId", { pagetitle: "userDetail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pagetitle: "editProfile" });
+export const changePassword = (req, res) =>
+  res.render("changen Password", { pagetitle: "changePassword" });
